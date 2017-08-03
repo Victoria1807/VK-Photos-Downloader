a06b990770cb22be83a4d8578949a224380ec25c
Ostap Pankevych <flamaster1807@gmail.com>
2017-08-03 12:14:18 +0300

    add a first version of script

---
+++ vk-photos-downloader.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3.5
+#-*- coding: UTF-8 -*-
+
+import vk, os, time
+from urllib.request import urlretrieve
+
+#Authorization
+session = vk.Session(access_token='9553717f54b665bc560b6a2cfa89fae62b43f1d442dcb3af337c5ffbc53005b99baf67e64f620dc351cf6')
+vk_api = vk.API(session)
+
+count = 0  # count of down. photos
+perc = 0  # percent of down. photos
+breaked = 0  # unsuccessful down.
+time_now = time.time()  # current time
+
+url = input("Enter a URL of album: ")  # url of album
+folder_name = input("Enter a name of folder for download photos: ")  # fold. for photo
+
+owner_id = url.split('album')[1].split('_')[0]  # id of owner
+album_id = url.split('album')[1].split('_')[1][0:-1]  # id of album
+photos_count = vk_api.photos.getAlbums(owner_id=owner_id, album_ids=album_id)[0]['size']  # count of ph. in albums
+album_title = vk_api.photos.getAlbums(owner_id=owner_id, album_ids=album_id)[0]['title']  # albums title
+photos_information = vk_api.photos.get(owner_id=owner_id, album_id=album_id)  # dictionaries of photos information
+
+photos_link = []  #  photos link
+
+for i in photos_information:
+    photos_link.append(i['src_xxbig'])
+
+if not os.path.exists(folder_name):
+    os.makedirs(folder_name + '/' + album_title)  # creating a folder for download photos
+    qw = 'ok'
+else:
+    print("A folder with this name already exists!")
+    exit()
+
+photo_name = 0  # photo name
+
+for i in photos_link:
+    photo_name += 1
+    urlretrieve(i, folder_name + '/' + album_title + '/' + str(photo_name) + '.jpg')  # download photos
+
+
